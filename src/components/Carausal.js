05e396d472c52866f2755387c14af89837fbe070
ravi barla <ravibarla1@gmail.com>
2024-01-18 17:49:39 +0530

    adding navbar, carausal and footer

---
+++ src/components/Carausal.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+export default function Carausal() {
+  return (
+    <div>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+        style={{objectFit:"contain !important"}}
+      >
+        <div className="carousel-inner">
+          {/* <div className="carousel-item active"> */}
+            <div
+              className="carousel-caption d-none d-md-block"
+              style={{ zIndex: "10" }}
+            >
+              <form className="d-flex">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0 bg-success"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </form>
+            </div>
+          {/* </div> */}
+          <div className="carousel-item active">
+            <img
+              src="https://source.unsplash.com/random/900×700/?burger"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900×700/?momos"
+              className="d-block w-100"
+              style={{ filter: "brightness(50%)" }}
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900×700/?pizza"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img className="card-img-top" alt="Card image cap" src="https://source.unsplash.com/random/300×300/?chair" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">some thin alcmal.</p>
+
+          <div className="container w-100">
+            <select className="m-2 h-100  bg-success">
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                );
+              })}
+            </select>
+            <select className="m-2 h-100 bg-success rounded">
+              <option value="half">half</option>
+              <option value="full">full</option>
+            </select>
+            <div className="d-inline h-100">total price</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
