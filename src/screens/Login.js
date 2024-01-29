ce9d3ae5d63a986b652163270068132fcbe92ad3
ravi barla <ravibarla1@gmail.com>
2024-01-30 00:10:33 +0530

    adding login component

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,72 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 export default function Login() {
+  const [credential, setCredential] = useState({
+    email: "",
+    password: "",
+  });
+  let navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:4000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credential.email,
+        password: credential.password,
+      }),
+    });
+    const jsonData = await response.json();
+    console.log(jsonData);
+    if (jsonData.success) {
+      // alert("Login successfully");
+      navigate("/");
+    } else {
+      alert("invalid username or password");
+    }
+  };
+  const onChange = (e) => {
+    setCredential({ ...credential, [e.target.name]: e.target.value });
+  };
   return (
-    <div>Login</div>
-  )
+    <>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="email"
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="password"
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+
+        <button type="submit" className="m-3 btn btn-success">
+          Submit
+        </button>
+        <Link to="/signup" className="m-3 btn btn-danger">
+          I Am A New User
+        </Link>
+      </form>
+    </>
+  );
 }
