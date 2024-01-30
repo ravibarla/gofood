4120852c2de9b0d21783f20021f1e329cb905c72
ravi barla <ravibarla1@gmail.com>
2024-01-30 12:36:42 +0530

    adding jwt authorization

--- backend/routes/createUser.js
+++ backend/routes/createUser.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const getAllUsers = require("../controller/controller");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwtSecret="freeendtoend"
 router.get("/", getAllUsers);
 router.post(
   "/createUser",
@@ -13,18 +16,18 @@ router.post(
   ],
 
   async (req, res) => {
-    console.log("inside register");
-    console.log("body :", req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log("body :", req.body);
     const { name, password, email, location } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(password, salt);
+
     try {
       const newUser = new User({
         name: name,
-        password: password,
+        password: secPassword,
         email: email,
         location: location,
       });
@@ -45,16 +48,27 @@ router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     // const data =
-    await User.find({ email }).then((data) => {
-      console.log(data);
-      console.log("password :" + data[0].password);
-      if (data[0].password === password) {
-        console.log("password matched");
-        return res.status(200).json({ success: "login successfully" });
-      } else {
-        console.log("password mismatches");
+    await User.find({ email }).then(async (data) => {
+      // console.log(data);
+      // console.log("password :" + data[0].password);
+      const pwdCompare = await bcrypt.compare(password, data[0].password);
+      if (!pwdCompare) {
         return res.status(400).json({ errors: "password mismatches" });
       }
+      const data1={
+        user:{
+          id:data[0].id
+        }
+      }
+      const authToken=jwt.sign(data1,jwtSecret)
+      return res.status(200).json({ success: "login successfully",authToken:authToken });
+      // if (data[0].password === password) {
+      //   console.log("password matched");
+      //   return res.status(200).json({ success: "login successfully" });
+      // } else {
+      //   console.log("password mismatches");
+      //   return res.status(400).json({ errors: "password mismatches" });
+      // }
     });
     // console.log("data :", data);
   } catch (err) {
