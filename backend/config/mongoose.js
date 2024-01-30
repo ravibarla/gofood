806d2d1a24cbaaa4b345a6352f37151815cc7da1
ravi barla <ravibarla1@gmail.com>
2024-01-30 18:00:49 +0530

    adding to get collection data

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 const mongoURI = `mongodb+srv://ravibarla1:Py4eEMLzyjpk0NeN@cluster0.vnsn4kw.mongodb.net/goFoodMern?retryWrites=true&w=majority`;
-
-
+// const mongoURI=`mongodump --uri mongodb+srv://ravibarla1:Py4eEMLzyjpk0NeN@cluster0.vnsn4kw.mongodb.net/goFoodMern`
 const mongoDB = async () => {
   await mongoose
-    .connect(mongoURI)
+    .connect(mongoURI, {
+      useNewUrlParser: true,
+    })
     .then(async () => {
       console.log("connected");
+      // const fetchedData = await mongoose.connection.db.collections("foodData");
+      // fetchedData.find({}).toArray((err, data) => {
+      //   if (err) {
+      //     console.log("err :", err);
+      //   } else {
+      //     console.log("data: ", data);
+      //   }
+      // });
     })
     .catch((err) => console.log("not connected"));
 };
